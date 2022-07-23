19e7d20f5a605dc93013337258aeac409df9f8d8
OnanThairu <thairuonan@gmail.com>
2022-07-23 12:47:36 +0300

    useContext

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { ThemeContext } from "../context/theme"
 
-function DarkModeToggle({ theme, setTheme }) {
+function DarkModeToggle() {
+  const [theme, setTheme ] = useState(ThemeContext)
+  
   function handleToggleTheme(e) {
     setTheme(e.target.checked ? "dark" : "light");
   }
